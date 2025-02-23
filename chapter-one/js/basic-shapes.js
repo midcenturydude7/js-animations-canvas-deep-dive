7c6ba7294dee34df184296058550145f32cef042
midCenturyDude7 <mgriffes@gmail.com>
2025-02-22 21:34:48 -0800

    Setting up structure to draw a line with a bezier curve

--- chapter-one/js/basic-shapes.js
+++ chapter-one/js/basic-shapes.js
@@ -4,25 +4,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.fillStyle = "red";
-ctx.beginPath();
-ctx.moveTo(10, 30);
-ctx.bezierCurveTo(50, 200, 640, -90, 800, 50);
-ctx.lineTo(800, 200);
-ctx.lineTo(10, 200);
-ctx.closePath();
-ctx.fill();
-ctx.lineWidth = 3;
-ctx.strokeStyle = "black";
+const colorRed = "rgba(255, 26, 14, 1)";
+const colorBlue = "rgba(54, 162, 235, 1)";
+const colorYellow = "rgba(255, 206, 86, 1)";
+const colorGreen = "rgba(75, 192, 192, 1)";
+const colorPurple = "rgba(153, 102, 255, 1)";
+const colorOrange = "rgba(255, 159, 64, 1)";
+const colorBlack = "rgba(0, 0, 0, 1)";
+const colorWhite = "rgba(255, 255, 255, 1)";
+
+const radius = 5;
+const angle = Math.PI / 180;
+
+ctx.beginPath(); // Starting point
+ctx.strokeStyle = colorRed;
+ctx.lineWidth = 10;
+ctx.arc(200, 300, radius, angle * 0, angle * 360, false); // Starting point
 ctx.stroke();
-
-ctx.fillStyle = "blue";
-ctx.font = "30px Source Code Pro";
-ctx.fillText("Hello World!", 100, 150);
-
-const grad = ctx.createLinearGradient(200, 100, 500, 500);
-grad.addColorStop(0, "white");
-grad.addColorStop(0.5, "red");
-grad.addColorStop(1, "black");
-ctx.fillStyle = grad;
-ctx.fillRect(100, 300, 200, 200);
